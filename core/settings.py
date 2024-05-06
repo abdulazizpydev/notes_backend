3e83fb70ba07d93399a4c6ff9a72596db85dd780
Abdulaziz <abdulazizkomilov2001@gmail.com>
2024-05-06 07:09:18 +0500

    deployment

--- core/settings.py
+++ core/settings.py
@@ -1,17 +1,16 @@
 import os
 from pathlib import Path
-
+from dotenv import load_dotenv
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+load_dotenv()
 
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
-SECRET_KEY = "django-insecure-dc3fi7s42(0fy^82@e^onkp_mk_v%=zl4yc7b)074*3a5264f$"
+DEBUG = bool(os.environ.get('DEBUG'))
 
-
-DEBUG = True
-
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',')
 
 
 
@@ -33,6 +32,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     "django.contrib.sessions.middleware.SessionMiddleware",
     "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -42,7 +42,21 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-CORS_ALLOW_ALL_ORIGINS = True
+
+SITE_ID = 1
+
+
+CORS_ORIGIN_WHITELIST = [
+    
+]
+
+CSRF_TRUSTED_ORIGINS = [
+    
+]
+
+CORS_ALLOWED_ORIGINS = [
+    
+]
 
 
 ROOT_URLCONF = "core.urls"
@@ -63,8 +77,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = "core.wsgi.application"
-
+WSGI_APPLICATION = "core.wsgi.app"
 
 
 
@@ -74,6 +87,17 @@ DATABASES = {
         "NAME": BASE_DIR / "db.sqlite3",
     }
 }
+
+# DATABASES = {
+#     "default": {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         "NAME": os.environ["DBNAME"],
+#         "USER": os.environ["DBUSER"],
+#         "PASSWORD": os.environ["DBPASSWORD"],
+#         "HOST": os.environ["DBHOST"],
+#         "PORT": os.environ["DBPORT"],
+#     }
+# }
 
 
 
@@ -104,11 +128,12 @@ USE_I18N = True
 
 USE_TZ = True
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
-
-
-STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_URL = "/static/"
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
 
 
